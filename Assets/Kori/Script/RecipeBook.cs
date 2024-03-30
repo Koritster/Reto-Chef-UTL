bea3f5f3df94fa8bc3f2f7dd449da113e358a40c
Koritster <64407378+Koritster@users.noreply.github.com>
2024-03-30 02:38:15 -0600

    Recipe Book
    V1 of Recipe Book

---
+++ Assets/Kori/Script/RecipeBook.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class RecipeBook : MonoBehaviour
+{
+    public GameObject[] pagesPanels;
+
+    [SerializeField] private GameObject backBtn, nextBtn;
+    [SerializeField] private int index = 0;
+
+
+    private void Awake()
+    {
+        index = 0;
+        
+        pagesPanels = GameObject.FindGameObjectsWithTag("Page");
+        pagesPanels = pagesPanels.OrderBy(x => x.name).ToArray();
+        
+        //Cerrar todo al inicio sin animación
+        foreach(var page in pagesPanels)
+        {
+            page.SetActive(false);
+        }
+
+        ComprobarInicioFin();
+        AbrirPagina(index);
+    }
+
+    private void CerrarPaginas()
+    {
+        foreach (var page in pagesPanels)
+        {
+            if(page == pagesPanels[index])
+            {
+                continue;
+            }
+            if(page.activeSelf)
+            {
+                AnimacionCerrarHijos(page);
+                StartCoroutine(anim_CerrarPaginas(page));
+            }
+        }
+    }
+
+    private void AnimacionCerrarHijos(GameObject p)
+    {
+        for(int i = 0; i < p.transform.childCount; i++)
+        {
+            p.transform.GetChild(i).GetComponentInChildren<Animator>(true).Play("Cerrar");
+        }
+    }
+
+    private void AnimacionabrirHijos(GameObject p)
+    {
+        for (int i = 0; i < p.transform.childCount; i++)
+        {
+            p.transform.GetChild(i).GetComponentInChildren<Animator>(true).Play("Abrir");
+        }
+    }
+
+    private void ComprobarInicioFin()
+    {
+        backBtn.SetActive(true);
+        nextBtn.SetActive(true);
+
+        if (index == 0)
+        {
+            backBtn.SetActive(false);
+        }
+        else if(index == (pagesPanels.Length - 1))
+        {
+            nextBtn.SetActive(false);
+        }
+    }
+
+    public void AbrirPagina(int pageNo)
+    {
+        index = pageNo;
+        
+        CerrarPaginas();
+
+        pagesPanels[index].SetActive(true);
+
+        AnimacionabrirHijos(pagesPanels[index]);
+        ComprobarInicioFin();
+    }
+
+    public void SiguientePagina()
+    {
+        AbrirPagina(index + 1);
+    }
+
+    public void AnteriorPagina()
+    {
+        AbrirPagina(index - 1);
+    }
+
+    IEnumerator anim_CerrarPaginas(GameObject p)
+    {
+        yield return new WaitForSeconds(1f);
+        p.SetActive(false);
+    }
+}
