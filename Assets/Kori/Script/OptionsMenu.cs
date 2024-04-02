985520ff0adf71ff01d4608ad50821c06e20058f
Koritster <64407378+Koritster@users.noreply.github.com>
2024-04-01 22:31:55 -0600

    Alpha 0.0.32
    - Book scripts added
    - Pause Menu

--- Assets/Kori/Script/OptionsMenu.cs
+++ Assets/Kori/Script/OptionsMenu.cs
@@ -5,14 +5,43 @@ using UnityEngine;
 public class OptionsMenu : MonoBehaviour
 {
     [SerializeField] private GameObject cp_Joysticks, cp_Buttons, c_Pause, p_Pause, p_Book;
+    private RecipeBook s_RecipeBook;
+
+    private void Awake()
+    {
+        p_Pause.SetActive(false);
+        p_Book.SetActive(false);
+        c_Pause.SetActive(false);
+
+        s_RecipeBook = GetComponent<RecipeBook>();
+    }
 
     public void OpenOptionsMenu()
     {
-
+        cp_Buttons.SetActive(false);
+        cp_Joysticks.SetActive(false);
+        c_Pause.SetActive(true);
+        p_Pause.SetActive(true);
     }
 
     public void CloseOptionsMenu()
     {
+        p_Pause.SetActive(false);
+        c_Pause.SetActive(false);
+        cp_Buttons.SetActive(true);
+        cp_Joysticks.SetActive(true);
+    }
 
+    public void AbrirLibro()
+    {
+        p_Pause.SetActive(false);
+        p_Book.SetActive(true);
+    }
+
+    public void CerrarLibro()
+    {
+        s_RecipeBook.AbrirPagina(0);
+        p_Book.SetActive(false);
+        p_Pause.SetActive(true);
     }
 }
